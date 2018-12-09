9171d6da8dde0ecc82fc477b1efd4d0c1691bb2f
Ariel Rossi Sapatelli <arielsapatelli@gmail.com>
2018-12-09 20:46:28 -0200

    criaçao do projeto

---
+++ src/main/java/com/demo/walletui/WalletUiApplication.java
@@ -0,0 +1,13 @@
+package com.demo.walletui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletUiApplication.class, args);
+	}
+}
+ 
